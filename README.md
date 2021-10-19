f678b22718b86ffce5f1ccb12067691574dd1d54
minhpn <minhpn@tinhvan.com>
2021-10-19 22:09:14 +0700

    :tada:

---
+++ README.md
@@ -0,0 +1,9 @@
+# A New React App Without CRA
+
+Run dev `yarn start`
+
+## Warning
+
+## Running
+
+## Oversimplified Diagram
---
+++ config/webpack.common.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude:
+          /node_modules/,
+        use: {
+          loader:
+            'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-react',
+              '@babel/preset-env',
+            ],
+            plugins: [
+              '@babel/plugin-transform-runtime',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, '../build'),
+	 	clean: true,
+	},
+};
